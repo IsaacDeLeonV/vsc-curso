1de1a24fb5f0f0e61249ec6340622ab532f76f8d
IsaacDeLeon <deleonvargasisaac@gmail.com>
2024-08-21 15:23:11 -0600

    Test de curso de vscode

---
+++ archivo.py
@@ -0,0 +1,9 @@
+print('Hola, mi nombre es Isaac')
+
+print('Hola, mi nombre es Isaac')
+<li><a href='res++'>link de isaac</a></li>
+
+print('Hola, mi nombre es Isaac')
+<li><a href='res++'>link isaac</a></li>
+
+
